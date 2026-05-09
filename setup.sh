3ecc4fca7149f1fc3d6c6ce895c37e96ed7219d7
Dan Mallinger <589101+dmallinger@users.noreply.github.com>
2026-05-08 20:14:36 -0400

    enabling auto-updating and rebooting at 4am

--- setup.sh
+++ setup.sh
@@ -10,6 +10,13 @@ fi
 apt -y update
 apt -y upgrade
 
+# Set up auto updates
+apt install -y unattended-upgrades
+dpkg-reconfigure -f noninteractive --priority=low unattended-upgrades
+sed -i 's/^.*Unattended-Upgrade::Automatic-Reboot "false".*$/Unattended-Upgrade::Automatic-Reboot "true";/g' /etc/apt/apt.conf.d/50unattended-upgrades
+sed -i 's/^.*Unattended-Upgrade::Automatic-Reboot-WithUsers.*$/Unattended-Upgrade::Automatic-Reboot-WithUsers "true";/g' /etc/apt/apt.conf.d/50unattended-upgrades
+sed -i 's/^.*Unattended-Upgrade::Automatic-Reboot-Time.*$/Unattended-Upgrade::Automatic-Reboot-Time "04:00";/g' /etc/apt/apt.conf.d/50unattended-upgrades
+
 # Other packages we rely on
 apt install -y screen tmux xscreensaver
 apt install -y vim curl git npm
